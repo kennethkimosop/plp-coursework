b7cb5e5babdd8ddc51b3bc93ebfc1aa9296aafd9
Kenneth <kenkimosop6@gmail.com>
2025-08-21 20:57:54 +0300

    Added week 3 Assignment discount calculation functions

---
+++ assignment3-functions/discount.py
@@ -0,0 +1,18 @@
+def calculate_discount(price,discount_percent):
+  discount = 0.2 * price
+  if discount_percent >= 20:
+    discount_amount = (discount_percent / 100) * price
+    final_price = price - discount_amount
+    return final_price
+  else:
+    return price # no discount if less than 20 percent
+  
+original_price = float(input("ENter the original price of the item: "))
+discount_percent = float(input("Enter the discount percentage: "))
+
+
+final_price = calculate_discount(original_price, discount_percent)
+if discount_percent >= 20:
+  print(f"Final price after {discount_percent} % disocunt is: {final_price}")
+else:
+  print(f"No discount applied. Original price: {final_price}")
